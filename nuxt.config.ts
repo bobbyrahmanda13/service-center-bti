9fae8c3e5484d0232fc39e7a7e2842db6370b557
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2026-03-05 06:22:16 +0700

    add nuxt/icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui','@nuxt/icon'],
   css:['~/assets/css/main.css'],
-  colorMode: {
-    preference: 'system',     // default: 'system' | 'light' | 'dark'
-    fallback: 'light',        // fallback jika tidak ada system preference
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',          // class prefix (default: '')
-    classSuffix: '',          // class suffix (default: '')
-    storageKey: 'nuxt-color-mode', // localStorage key
-  },
+  // colorMode: {
+  //   preference: 'system',     // default: 'system' | 'light' | 'dark'
+  //   fallback: 'light',        // fallback jika tidak ada system preference
+  //   hid: 'nuxt-color-mode-script',
+  //   globalName: '__NUXT_COLOR_MODE__',
+  //   componentName: 'ColorScheme',
+  //   classPrefix: '',          // class prefix (default: '')
+  //   classSuffix: '',          // class suffix (default: '')
+  //   storageKey: 'nuxt-color-mode', // localStorage key
+  // },
 
 })
